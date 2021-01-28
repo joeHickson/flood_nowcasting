35de1da30daf302f4b8a5ec2f5d28949ca9a051d
Joe Hickson <joeHickson@users.noreply.github.com>
2021-01-28 12:44:17 +0000

    including setup file

---
+++ setup.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+with open('README.md') as f:
+    readme = f.read()
+
+with open('LICENSE') as f:
+    license = f.read()
+
+setup(
+    name='flood_nowcasting',
+    version='0.1.0',
+    description='Flood nowcasting script',
+    long_description=readme,
+    author='Joe Hickson',
+    url='https://github.com/joehickson/flood_nowcasting',
+    license=license,
+    packages=find_packages(exclude=('tests'))
+)
