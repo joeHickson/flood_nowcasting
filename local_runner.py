ffc650977d4ed5eafa6d07ee379e772666b74f89
Joe Hickson <joeHickson@users.noreply.github.com>
2021-11-26 11:45:18 +0000

    bit more logging

--- local_runner.py
+++ local_runner.py
@@ -9,11 +9,13 @@ def load_config():
 
 
 if __name__ == '__main__':
-
+    print("running")
     config = load_config()
 
     nowcast = FloodNowcasting(app_key=config['APP_KEY'],
                               app_secret=config['APP_SECRET'],
                               access_token=config['ACCESS_TOKEN'],
-                              access_token_secret=config['ACCESS_TOKEN_SECRET'])
+                              access_token_secret=config[
+                                  'ACCESS_TOKEN_SECRET'])
     nowcast.main()
+    print("run complete")
