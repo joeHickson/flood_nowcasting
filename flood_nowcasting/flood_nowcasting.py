1e85a414d1348f223bb4cda7d65ee9d6483624f1
Joe Hickson <joeHickson@users.noreply.github.com>
2021-11-26 12:04:01 +0000

    change object structure on tweepy exception

--- flood_nowcasting/flood_nowcasting.py
+++ flood_nowcasting/flood_nowcasting.py
@@ -191,7 +191,7 @@ class FloodNowcasting:
         try:
             self.api.update_status(status=message)
         except tweepy.errors.TweepyException as error:
-            if error.api_code == 187:  # Status is a duplicate
+            if 187 in error.api_codes:  # Status is a duplicate
                 pass
             else:
                 raise
